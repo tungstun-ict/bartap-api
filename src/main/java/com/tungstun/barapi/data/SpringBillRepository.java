3ef75d9b88e05515edb20da778267745c0391d43
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2020-12-28 14:41:39 +0100

    -add bill repository -cleanup bar repository

---
+++ src/main/java/com/tungstun/barapi/data/SpringBillRepository.java
@@ -0,0 +1,10 @@
+package com.tungstun.barapi.data;
+
+import com.tungstun.barapi.domain.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringBillRepository extends JpaRepository<Bill, Long>{
+    Optional<Bill> findById(Long id);
+}
