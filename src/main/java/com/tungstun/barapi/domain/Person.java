a5ecf25bc0d2bfaa9bfff0195c4db0db2a7e18d6
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-01 15:14:26 +0100

    Add id as JSON Identity Info on Person.class

--- src/main/java/com/tungstun/barapi/domain/Person.java
+++ src/main/java/com/tungstun/barapi/domain/Person.java
@@ -1,7 +1,14 @@
 package com.tungstun.barapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.persistence.*;
 
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Person {
