86ed3bad8149cb1859ea41442a23f5b9a136c790
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-07 20:40:46 +0100

    Add OrderLine
    Added OrderLine for multi-product Order

--- src/main/java/com/tungstun/barapi/domain/Order.java
+++ src/main/java/com/tungstun/barapi/domain/Order.java
@@ -1,15 +1,31 @@
 package com.tungstun.barapi.domain;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
+
+    @Column(name = "date")
     private Date date;
+
+    @Column(name = "amount")
     private int amount;
+
+    @Column(name = "price")
     private double price;
+
+    @Transient
     private Bartender bartender;
+
+    @Transient
     private Product product;
 
+    public Order() { }
     public Order(String id, Date date, int amount, double price, Bartender bartender, Product product) {
         this.id = id;
         this.date = date;
