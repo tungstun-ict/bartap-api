62a311b5a4f92f9d3f0e751d646951d4737fab39
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-08 21:25:55 +0100

    Change Date to LocalDateTime

--- src/main/java/com/tungstun/barapi/domain/Order.java
+++ src/main/java/com/tungstun/barapi/domain/Order.java
@@ -1,7 +1,7 @@
 package com.tungstun.barapi.domain;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -12,7 +12,7 @@ public class Order {
     private Long id;
 
     @Column(name = "date")
-    private Date date;
+    private LocalDateTime date;
 
     @Column(name = "price")
     private double price;
@@ -24,33 +24,22 @@ public class Order {
     private List<OrderLine> orderLines;
 
     public Order() { }
-    public Order(Long id, Date date, double price, Bartender bartender, List<OrderLine> orderLines) {
-        this.id = id;
+    public Order(LocalDateTime date, double price, Bartender bartender, List<OrderLine> orderLines) {
         this.date = date;
         this.price = price;
         this.bartender = bartender;
         this.orderLines = orderLines;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public Date getDate() {
-        return date;
-    }
+    public LocalDateTime getDate() { return date; }
 
-    public double getPrice() {
-        return price;
-    }
+    public double getPrice() { return price; }
 
-    public Bartender getBartender() {
-        return bartender;
-    }
+    public Bartender getBartender() { return bartender; }
 
-    public List<OrderLine> getOrderLines() {
-        return orderLines;
-    }
+    public List<OrderLine> getOrderLines() { return orderLines; }
 
     public boolean addOrderLine(OrderLine orderLine) {
         if( !this.orderLines.contains(orderLine) ) return this.orderLines.add(orderLine);
