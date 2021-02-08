b6a5906cd69efab88ab17575012be68d13ec826a
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-08 21:27:11 +0100

    Add order request fields

--- src/main/java/com/tungstun/barapi/presentation/dto/request/OrderRequest.java
+++ src/main/java/com/tungstun/barapi/presentation/dto/request/OrderRequest.java
@@ -1,4 +1,11 @@
 package com.tungstun.barapi.presentation.dto.request;
 
+import javax.validation.constraints.NotNull;
+
 public class OrderRequest {
+    @NotNull(message = "Customer ID cannot be empty")
+    public Long customerId;
+
+    @NotNull(message = "Customer ID cannot be empty")
+    public Long bartenderId;
 }
