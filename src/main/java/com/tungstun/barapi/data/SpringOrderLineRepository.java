5b0b29ab9880dd1e8e39980c712595b2ee381dcc
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-09 12:13:11 +0100

    Add order line repository class content

--- src/main/java/com/tungstun/barapi/data/SpringOrderLineRepository.java
+++ src/main/java/com/tungstun/barapi/data/SpringOrderLineRepository.java
@@ -1,4 +1,10 @@
 package com.tungstun.barapi.data;
 
-public interface SpringOrderLineRepository {
+import com.tungstun.barapi.domain.OrderLine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringOrderLineRepository extends JpaRepository<OrderLine, Long> {
+    Optional<OrderLine> findById(Long id);
 }
