a3d0eb792984cc7052aa0f0aea3f34c95ed60280
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-09 12:13:56 +0100

    Add getAllOrderLines API call

--- src/main/java/com/tungstun/barapi/application/OrderLineService.java
+++ src/main/java/com/tungstun/barapi/application/OrderLineService.java
@@ -1,4 +1,31 @@
 package com.tungstun.barapi.application;
 
+import com.tungstun.barapi.data.SpringOrderLineRepository;
+import com.tungstun.barapi.domain.Order;
+import com.tungstun.barapi.domain.OrderLine;
+import javassist.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class OrderLineService {
+    private SpringOrderLineRepository SPRING_ORDER_LINE_REPOSITORY;
+    private OrderService ORDER_SERVICE;
+
+    public OrderLineService(SpringOrderLineRepository springOrderLineRepository, OrderService orderService) {
+        this.SPRING_ORDER_LINE_REPOSITORY = springOrderLineRepository;
+        this.ORDER_SERVICE = orderService;
+    }
+
+    public List<OrderLine> getAllOrderLinesOfBar(Long barId) throws NotFoundException {
+        List<Order> orders = this.ORDER_SERVICE.getAllOrdersOfBar(barId);
+        List<OrderLine> orderLines = new ArrayList<>();
+        for (Order order : orders) {
+            orderLines.addAll(order.getOrderLines());
+        }
+        if (orderLines.isEmpty()) throw new NotFoundException(String.format("No order lines found for bar with id: %s", barId));
+        return orderLines;
+    }
 }
--- src/main/java/com/tungstun/barapi/presentation/controllers/OrderLineController.java
+++ src/main/java/com/tungstun/barapi/presentation/controllers/OrderLineController.java
@@ -1,4 +1,40 @@
 package com.tungstun.barapi.presentation.controllers;
 
+import com.tungstun.barapi.application.OrderLineService;
+import com.tungstun.barapi.domain.OrderLine;
+import com.tungstun.barapi.presentation.dto.response.OrderLineResponse;
+import com.tungstun.barapi.presentation.mapper.ResponseMapper;
+import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("bars/{barId}/")
 public class OrderLineController {
+    private final ResponseMapper RESPONSE_MAPPER;
+    private OrderLineService ORDER_LINE_SERVICE;
+
+    public OrderLineController(OrderLineService orderLineService, ResponseMapper responseMapper) {
+        this.ORDER_LINE_SERVICE = orderLineService;
+        this.RESPONSE_MAPPER = responseMapper;
+    }
+
+    private OrderLineResponse convertToOrderLineResponse(OrderLine orderLine){
+        return RESPONSE_MAPPER.convert(orderLine, OrderLineResponse.class);
+    }
+
+    @GetMapping("orderlines")
+    public ResponseEntity<List<OrderLineResponse>> getAllBarOrderLines(
+            @PathVariable("barId") Long barId
+    ) throws NotFoundException {
+        List<OrderLine> orderLines = this.ORDER_LINE_SERVICE.getAllOrderLinesOfBar(barId);
+        List<OrderLineResponse> orderLineResponses = RESPONSE_MAPPER.convertList(orderLines, OrderLineResponse.class);
+        return new ResponseEntity<>(orderLineResponses, HttpStatus.OK);
+    }
 }
