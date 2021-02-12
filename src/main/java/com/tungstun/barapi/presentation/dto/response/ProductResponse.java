a3b39ad014d901205078636946345221dc7f0888
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-12 13:59:56 +0100

    Add ProductResponse

---
+++ src/main/java/com/tungstun/barapi/presentation/dto/response/ProductResponse.java
@@ -0,0 +1,43 @@
+package com.tungstun.barapi.presentation.dto.response;
+
+import com.tungstun.barapi.domain.Category;
+
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String brand;
+    private double size;
+    private double price;
+    private boolean isFavorite;
+    private Category category;
+
+    public ProductResponse() { }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getBrand() { return brand; }
+
+    public void setBrand(String brand) { this.brand = brand; }
+
+    public double getSize() { return size; }
+
+    public void setSize(double size) { this.size = size; }
+
+    public double getPrice() { return price; }
+
+    public void setPrice(double price) { this.price = price; }
+
+    public boolean isFavorite() { return isFavorite; }
+
+    public void setFavorite(boolean favorite) { isFavorite = favorite; }
+
+    public Category getCategory() { return category; }
+
+    public void setCategory(Category category) { this.category = category; }
+}
