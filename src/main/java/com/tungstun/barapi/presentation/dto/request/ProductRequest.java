aed1721591febface93734bcee99dab974718afd
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-12 14:03:06 +0100

    Add ProductRequest

---
+++ src/main/java/com/tungstun/barapi/presentation/dto/request/ProductRequest.java
@@ -0,0 +1,19 @@
+package com.tungstun.barapi.presentation.dto.request;
+
+public class ProductRequest {
+    public Long id;
+
+    public String name;
+
+    public String brand;
+
+    public double size;
+
+    public double price;
+
+    public boolean isFavorite;
+
+    public Long categoryId;
+
+    public String productType;
+}
