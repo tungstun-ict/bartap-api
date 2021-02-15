2b1ee27c39ef3a725be8532cb0d7a40741bebfaf
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-15 17:35:05 +0100

    Add creationDate, closedDate, price to bill

--- src/main/java/com/tungstun/barapi/domain/Bill.java
+++ src/main/java/com/tungstun/barapi/domain/Bill.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @JsonIdentityInfo(
@@ -21,6 +22,15 @@ public class Bill {
     @Column(name = "is_payed")
     private boolean isPayed;
 
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "closed_date")
+    private LocalDateTime closedDate;
+
+    @Column(name = "price")
+    private double price;
+
     @JsonManagedReference
     @ManyToOne(optional = false)
     @JoinColumn(name = "session_id")
@@ -35,14 +45,16 @@ public class Bill {
             cascade = CascadeType.ALL,
             orphanRemoval = true
     )
-    private List<Order> orders;
+    private List<OrderLine> orderLines;
 
     public Bill() { }
-    public Bill(Session session, Customer customer, List<Order> orders, boolean isPayed) {
+    public Bill(Session session, Customer customer, List<OrderLine> orderLines, boolean isPayed, double price) {
         this.session = session;
         this.customer = customer;
-        this.orders = orders;
+        this.orderLines = orderLines;
         this.isPayed = isPayed;
+        this.price = price;
+        this.creationDate = LocalDateTime.now();
     }
 
     public Long getId() {
@@ -55,20 +67,30 @@ public class Bill {
         return customer;
     }
 
-    public List<Order> getOrders() {
-        return this.orders;
+    public List<OrderLine> getOrderLines() {
+        return this.orderLines;
     }
 
-    public boolean addBartender(Order order ){
-        if ( !this.orders.contains(order) ) return this.orders.add(order);
+    public boolean addOrderLine(OrderLine orderLine ){
+        if ( !this.orderLines.contains(orderLine) ) return this.orderLines.add(orderLine);
         return false;
     }
 
-    public boolean removeBartender(Order order){
-        return this.orders.remove(order);
+    public boolean removeOrderLine(Order order){
+        return this.orderLines.remove(order);
     }
 
     public boolean isPayed() { return isPayed; }
 
     public void setPayed(boolean payed) { isPayed = payed; }
+
+    public LocalDateTime getCreationDate() { return creationDate; }
+
+    public LocalDateTime getClosedDate() { return closedDate; }
+
+    public void setClosedDate(LocalDateTime closedDate) { this.closedDate = closedDate; }
+
+    public double getPrice() { return price; }
+
+    public void setPrice(double price) { this.price = price; }
 }
