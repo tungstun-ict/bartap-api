c56bcd8efe5e2acce1464e312c09c1aa937d2eee
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-16 10:50:31 +0100

    Add ProductType to CategoryResponse

--- src/main/java/com/tungstun/barapi/presentation/dto/response/CategoryResponse.java
+++ src/main/java/com/tungstun/barapi/presentation/dto/response/CategoryResponse.java
@@ -1,11 +1,14 @@
 package com.tungstun.barapi.presentation.dto.response;
 
+import com.tungstun.barapi.domain.product.ProductType;
+
 public class CategoryResponse {
     private Long id;
     private String name;
+    private ProductType productType;
 
     public CategoryResponse() { }
-    public CategoryResponse(Long id, String name) {
+    public CategoryResponse(Long id, String name, ProductType productType) {
         this.id = id;
         this.name = name;
     }
@@ -17,4 +20,8 @@ public class CategoryResponse {
     public String getName() { return name; }
 
     public void setName(String name) { this.name = name; }
+
+    public ProductType getProductType() { return productType; }
+
+    public void setProductType(ProductType productType) { this.productType = productType; }
 }
