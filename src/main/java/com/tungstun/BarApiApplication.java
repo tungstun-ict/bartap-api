d742b23d87ccd9aa9be817fbe6bd2cf7942caf6f
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-18 18:46:23 +0100

    Refactor move Spring application up one package

--- src/main/java/com/tungstun/BarApiApplication.java
+++ src/main/java/com/tungstun/BarApiApplication.java
@@ -1,8 +1,7 @@
-package com.tungstun.barapi;
+package com.tungstun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BarApiApplication {
