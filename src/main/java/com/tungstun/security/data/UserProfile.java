0cbffdffa12c6749ffa50bb5fe0f6339eb735d15
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-18 18:45:09 +0100

    Add User Add UserRole

---
+++ src/main/java/com/tungstun/security/data/UserProfile.java
@@ -0,0 +1,11 @@
+package com.tungstun.security.data;
+
+public class UserProfile {
+    private String username;
+
+    public UserProfile(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() { return username; }
+}
