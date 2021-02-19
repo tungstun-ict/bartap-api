0c82f85e963e88126fb3822aab7cd043578299a0
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-19 16:53:34 +0100

    Add SecurityConfig for security

---
+++ src/main/java/com/tungstun/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.tungstun.security;
+
+import com.tungstun.security.presentation.filter.JwtAuthenticationFilter;
+import com.tungstun.security.presentation.filter.JwtAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    public final static String LOGIN_PATH = "/login";
+    public final static String REGISTER_PATH = "/register";
+
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${security.jwt.expiration-in-ms}")
+    private Integer jwtExpirationInMs;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors()
+                .and()
+            .csrf()
+                .disable()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.POST, REGISTER_PATH).permitAll()
+                .antMatchers(HttpMethod.POST, LOGIN_PATH).permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .addFilterBefore(
+                    new JwtAuthenticationFilter(
+                            LOGIN_PATH,
+                            this.jwtSecret,
+                            this.jwtExpirationInMs,
+                            this.authenticationManager()
+                    ),
+                    UsernamePasswordAuthenticationFilter.class
+            )
+            .addFilter(new JwtAuthorizationFilter(this.jwtSecret, this.authenticationManager()))
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
