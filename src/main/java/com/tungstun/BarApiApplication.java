ecfcbb0c59c7f55d1bad257e2578600819e92ef9
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-05 16:08:18 +0100

    Add swagger to project requests Add API information to Swagger

--- src/main/java/com/tungstun/BarApiApplication.java
+++ src/main/java/com/tungstun/BarApiApplication.java
@@ -2,12 +2,13 @@ package com.tungstun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BarApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarApiApplication.class, args);
 	}
-
 }
