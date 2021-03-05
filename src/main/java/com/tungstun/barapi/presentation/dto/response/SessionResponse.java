14312d06334e0033da0f1f67454661078a0e750a
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-05 22:43:00 +0100

    Add Swagger annotations session response

--- src/main/java/com/tungstun/barapi/presentation/dto/response/SessionResponse.java
+++ src/main/java/com/tungstun/barapi/presentation/dto/response/SessionResponse.java
@@ -3,16 +3,28 @@ package com.tungstun.barapi.presentation.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.tungstun.barapi.domain.Bartender;
 import com.tungstun.barapi.domain.bill.Bill;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel(description = "Response details about the session")
 public class SessionResponse {
+    @ApiModelProperty(notes = "The session's id")
     private Long id;
+
+    @ApiModelProperty(notes = "The session's creation date")
     private LocalDateTime creationDate;
+
+    @ApiModelProperty(notes = "The date the session got close")
     private LocalDateTime closedDate;
+
+    @ApiModelProperty(notes = "The session's bartenders")
     private List<Bartender> bartenders;
+
+    @ApiModelProperty(notes = "The session's bills")
     private List<Bill> bills;
 
     public SessionResponse() { }
