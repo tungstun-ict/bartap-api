a9d2a430f1877a74de38985db9519b495ebbd7b6
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-05 22:42:51 +0100

    Add Swagger annotations for SessionController

--- src/main/java/com/tungstun/barapi/presentation/controllers/SessionController.java
+++ src/main/java/com/tungstun/barapi/presentation/controllers/SessionController.java
@@ -2,8 +2,11 @@ package com.tungstun.barapi.presentation.controllers;
 
 import com.tungstun.barapi.application.SessionService;
 import com.tungstun.barapi.domain.Session;
+import com.tungstun.barapi.presentation.dto.response.ProductResponse;
 import com.tungstun.barapi.presentation.dto.response.SessionResponse;
 import com.tungstun.barapi.presentation.mapper.ResponseMapper;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,67 +32,100 @@ public class SessionController {
     }
 
     @GetMapping
+    @ApiOperation(
+            value = "Finds all sessions of bar",
+            notes = "Provide id of bar to look up all sessions that are linked to the bar",
+            response = SessionResponse.class,
+            responseContainer = "List"
+    )
     public ResponseEntity<List<SessionResponse>> getAllBarSessions(
-            @PathVariable("barId") Long barId)
+            @ApiParam(value = "ID value for the bar you want to retrieve sessions from") @PathVariable("barId") Long barId)
             throws NotFoundException {
-
         List<Session> allSessions = this.SESSION_SERVICE.getAllSessionsOfBar(barId);
         List<SessionResponse> sessionResponses = RESPONSE_MAPPER.convertList(allSessions, SessionResponse.class);
         return new ResponseEntity<>(sessionResponses,  HttpStatus.OK);
     }
 
     @GetMapping(path = "/{sessionId}")
+    @ApiOperation(
+            value = "Finds session of bar",
+            notes = "Provide id of bar and session to look up the specific session of the bar",
+            response = SessionResponse.class
+    )
     public ResponseEntity<SessionResponse> getBarSessionsById(
-            @PathVariable("barId") Long barId,
-            @PathVariable("sessionId") Long sessionId) throws NotFoundException {
+            @ApiParam(value = "ID value for the bar you want to retrieve the session from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the session you want to retrieve") @PathVariable("sessionId") Long sessionId) throws NotFoundException {
         Session session = this.SESSION_SERVICE.getSessionOfBar(barId, sessionId);
         return new ResponseEntity<>(convertToSessionResult(session),  HttpStatus.OK);
     }
 
     @PostMapping
+    @ApiOperation(
+            value = "Creates new session for bar",
+            notes = "Provide id of bar to add a new session with information from the request body to the bar",
+            response = SessionResponse.class
+    )
     public ResponseEntity<SessionResponse> createNewSession(
-            @PathVariable("barId") Long barId)
+            @ApiParam(value = "ID value for the bar you want to create the session for") @PathVariable("barId") Long barId)
             throws NotFoundException {
-
         Session session = this.SESSION_SERVICE.createNewSession(barId);
         return new ResponseEntity<>(convertToSessionResult(session),  HttpStatus.CREATED);
     }
 
     @PatchMapping("/{sessionId}/end")
+    @ApiOperation(
+            value = "End the session of bar",
+            notes = "Provide id of bar and session to close the session of the bar",
+            response = SessionResponse.class
+    )
     public ResponseEntity<SessionResponse> endSession(
-            @PathVariable("barId") Long barId,
-            @PathVariable("sessionId") Long sessionId
+            @ApiParam(value = "ID value for the bar you want to end the session from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the session you want to end") @PathVariable("sessionId") Long sessionId
     ) throws NotFoundException {
         Session session = this.SESSION_SERVICE.endSession(barId, sessionId);
         return new ResponseEntity<>(convertToSessionResult(session),  HttpStatus.OK);
     }
 
     @PatchMapping("/{sessionId}/bartenders/{bartenderId}/add")
+    @ApiOperation(
+            value = "Adds bartender to the session of bar",
+            notes = "Provide id of bar, session and bartender to add the bartender to the session of the bar",
+            response = SessionResponse.class
+    )
     public ResponseEntity<SessionResponse> addBartenderToSession(
-            @PathVariable("barId") Long barId,
-            @PathVariable("sessionId") Long sessionId,
-            @PathVariable("bartenderId") Long bartenderId
+            @ApiParam(value = "ID value for the bar you want to add the bartend to") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the session you want to add the bartend to") @PathVariable("sessionId") Long sessionId,
+            @ApiParam(value = "ID value for the bartender you want to add") @PathVariable("bartenderId") Long bartenderId
     ) throws NotFoundException {
         Session session = this.SESSION_SERVICE.addBartenderToSession(barId, sessionId, bartenderId);
         return new ResponseEntity<>(convertToSessionResult(session),  HttpStatus.OK);
     }
 
     @PatchMapping("/{sessionId}/bartenders/{bartenderId}/remove")
+    @ApiOperation(
+            value = "Removes bartender from the session of bar",
+            notes = "Provide id of bar, session and bartender to remove the bartender from the session of the bar",
+            response = SessionResponse.class
+    )
     public ResponseEntity<SessionResponse> removeBartenderFromSession(
-            @PathVariable("barId") Long barId,
-            @PathVariable("sessionId") Long sessionId,
-            @PathVariable("bartenderId") Long bartenderId
+            @ApiParam(value = "ID value for the bar you want to remove the bartend from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the session you want to remove the bartend from") @PathVariable("sessionId") Long sessionId,
+            @ApiParam(value = "ID value for the bartender you want to remove") @PathVariable("bartenderId") Long bartenderId
     ) throws NotFoundException {
         Session session = this.SESSION_SERVICE.removeBartenderFromSession(barId, sessionId, bartenderId);
         return new ResponseEntity<>(convertToSessionResult(session),  HttpStatus.OK);
     }
 
     @DeleteMapping("/{sessionId}")
+    @ApiOperation(
+            value = "Deletes the session of bar",
+            notes = "Provide id of bar and session to delete the session from the bar",
+            response = ProductResponse.class
+    )
     public ResponseEntity<Void> deleteSession(
-            @PathVariable("barId") Long barId,
-            @PathVariable("sessionId") Long sessionId)
+            @ApiParam(value = "ID value for the bar you want to delete the session from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the session you want to delete") @PathVariable("sessionId") Long sessionId)
             throws NotFoundException {
-
         this.SESSION_SERVICE.deleteSession(barId, sessionId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
