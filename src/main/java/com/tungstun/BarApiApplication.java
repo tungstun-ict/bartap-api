676999b4ccf8c3004d4e32d8a49531407209b7a4
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-05 16:44:28 +0100

    Add @EnableOpenApi annotation to enable open api

--- src/main/java/com/tungstun/BarApiApplication.java
+++ src/main/java/com/tungstun/BarApiApplication.java
@@ -2,10 +2,12 @@ package com.tungstun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableOpenApi
 public class BarApiApplication {
 
 	public static void main(String[] args) {
