0d9fdd4ae8908cde02bad240be644559868ccacb
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-05 20:01:36 +0100

    Add Swagger annotations CategoryController

--- src/main/java/com/tungstun/barapi/presentation/controllers/CategoryController.java
+++ src/main/java/com/tungstun/barapi/presentation/controllers/CategoryController.java
@@ -5,6 +5,8 @@ import com.tungstun.barapi.domain.Category;
 import com.tungstun.barapi.presentation.dto.request.CategoryRequest;
 import com.tungstun.barapi.presentation.dto.response.CategoryResponse;
 import com.tungstun.barapi.presentation.mapper.ResponseMapper;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,9 +34,15 @@ public class CategoryController {
     }
 
     @GetMapping
+    @ApiOperation(
+            value = "Finds all categories of bar",
+            notes = "Provide id of bar to look up all categories that are linked to the bar",
+            response = CategoryResponse.class,
+            responseContainer = "List"
+    )
     public ResponseEntity<List<CategoryResponse>> getCategoriesOfBar(
-            @PathVariable("barId") Long barId,
-            @RequestParam(value = "productType", required = false) String productType
+            @ApiParam(value = "ID value for the bar you want to retrieve categories from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "(Optional) Product type of categories you want to retrieve") @RequestParam(value = "productType", required = false) String productType
     ) throws NotFoundException {
         List<Category> categories = this.CATEGORY_SERVICE.getCategoriesOfBar(barId, productType);
         List<CategoryResponse> categoryResponses = RESPONSE_MAPPER.convertList(categories, CategoryResponse.class);
@@ -42,17 +50,27 @@ public class CategoryController {
     }
 
     @GetMapping("/{categoryId}")
+    @ApiOperation(
+            value = "Finds category of bar",
+            notes = "Provide id of bar and category to look up the specific category from the bar",
+            response = CategoryResponse.class
+    )
     public ResponseEntity<CategoryResponse> getCategoryOfBar(
-            @PathVariable("barId") Long barId,
-            @PathVariable("categoryId") Long categoryId
+            @ApiParam(value = "ID value for the bar you want to retrieve the category from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the category you want to retrieve") @PathVariable("categoryId") Long categoryId
     ) throws NotFoundException {
         Category category = this.CATEGORY_SERVICE.getCategoryOfBar(barId, categoryId);
         return new ResponseEntity<>(convertToCategoryResult(category), HttpStatus.OK);
     }
 
     @PostMapping
+    @ApiOperation(
+            value = "Creates new category for bar",
+            notes = "Provide id of bar to add new category with information from the request body to the bar",
+            response = CategoryResponse.class
+    )
     public ResponseEntity<CategoryResponse> addCategoryToBar(
-            @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the bar you want to create a new category for") @PathVariable("barId") Long barId,
             @Valid @RequestBody CategoryRequest categoryRequest
     ) throws NotFoundException {
         Category category = this.CATEGORY_SERVICE.addCategoryToBar(barId, categoryRequest);
@@ -60,9 +78,14 @@ public class CategoryController {
     }
 
     @PutMapping("/{categoryId}")
+    @ApiOperation(
+            value = "Updates the category of bar",
+            notes = "Provide id of bar to update the category with information from the request body",
+            response = CategoryResponse.class
+    )
     public ResponseEntity<CategoryResponse> updateCategoryOfBar(
-            @PathVariable("barId") Long barId,
-            @PathVariable("categoryId") Long categoryId,
+            @ApiParam(value = "ID value for the bar you want to update the category from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the category you want to update") @PathVariable("categoryId") Long categoryId,
             @Valid @RequestBody CategoryRequest categoryRequest
     ) throws NotFoundException {
         Category category = this.CATEGORY_SERVICE.updateCategoryOfBar(barId, categoryId, categoryRequest);
@@ -70,9 +93,14 @@ public class CategoryController {
     }
 
     @DeleteMapping("/{categoryId}")
+    @ApiOperation(
+            value = "Deletes the category of bar",
+            notes = "Provide id of bar to delete the category of bar",
+            response = CategoryResponse.class
+    )
     public ResponseEntity<Void> deleteCategoryOfBar(
-            @PathVariable("barId") Long barId,
-            @PathVariable("categoryId") Long categoryId
+            @ApiParam(value = "ID value for the bar you want to delete the category from") @PathVariable("barId") Long barId,
+            @ApiParam(value = "ID value for the category you want to delete") @PathVariable("categoryId") Long categoryId
     ) throws NotFoundException {
         this.CATEGORY_SERVICE.deleteCategoryFromBar(barId, categoryId);
         return new ResponseEntity<>(HttpStatus.OK);
