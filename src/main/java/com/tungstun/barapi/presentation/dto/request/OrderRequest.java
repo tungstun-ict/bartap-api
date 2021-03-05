7f4bff4f6fb06f71ab8af66d103cca7aff99f6e0
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-05 20:32:01 +0100

    Add Swagger annotations order response/request

--- src/main/java/com/tungstun/barapi/presentation/dto/request/OrderRequest.java
+++ src/main/java/com/tungstun/barapi/presentation/dto/request/OrderRequest.java
@@ -1,13 +1,20 @@
 package com.tungstun.barapi.presentation.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotNull;
 
+@ApiModel(description = "Request details about the order")
 public class OrderRequest {
-    @NotNull(message = "Customer ID cannot be empty")
+    @ApiModelProperty(notes = "The ID of the order's bartender")
+    @NotNull
     public Long bartenderId;
 
-    @NotNull(message = "Product ID cannot be empty")
+    @ApiModelProperty(notes = "The ID of the order's product")
+    @NotNull
     public Long productId;
 
+    @ApiModelProperty(notes = "The order's amount of products")
     public Integer amount;
 }
