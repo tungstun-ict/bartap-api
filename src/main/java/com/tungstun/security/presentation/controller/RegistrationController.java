13c1e49068bf3eb050a124c897ba67ddaef93aab
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-07 23:30:22 +0100

    Split login and register into seperate controllers

--- src/main/java/com/tungstun/security/presentation/controller/RegistrationController.java
+++ src/main/java/com/tungstun/security/presentation/controller/RegistrationController.java
@@ -1,10 +1,8 @@
 package com.tungstun.security.presentation.controller;
 
 import com.tungstun.security.application.UserService;
-import com.tungstun.security.presentation.dto.request.LoginRequest;
 import com.tungstun.security.presentation.dto.request.UserRegistrationRequest;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +10,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.security.auth.login.LoginException;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/users")
-public class UserController {
+@RequestMapping("/api")
+public class RegistrationController {
     private final UserService USER_SERVICE;
 
-    public UserController(UserService userService) { this.USER_SERVICE = userService; }
+    public RegistrationController(UserService userService) { this.USER_SERVICE = userService; }
 
     @PostMapping("/register")
     @ApiOperation(
@@ -34,19 +31,4 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
-    @ApiOperation(
-            value = "Logs in user",
-            notes = "Provide login credentials in the request body to receive a session token"
-    )
-    public ResponseEntity<Void> login(
-            @Valid @RequestBody LoginRequest loginRequest
-    ) throws LoginException {
-        String jwt = this.USER_SERVICE.loginUser(loginRequest);
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Authorization", jwt);
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body(null);
-    }
 }
