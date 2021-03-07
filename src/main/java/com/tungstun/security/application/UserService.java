bc8f7d4b9f316a99ce10fb3608a635afe08fd0f1
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-07 20:03:32 +0100

    Add login API route Move login to userService

--- src/main/java/com/tungstun/security/application/UserService.java
+++ src/main/java/com/tungstun/security/application/UserService.java
@@ -3,27 +3,85 @@ package com.tungstun.security.application;
 import com.tungstun.security.data.SpringUserRepository;
 import com.tungstun.security.data.User;
 import com.tungstun.security.data.UserRole;
+import com.tungstun.security.presentation.dto.request.LoginRequest;
+import com.tungstun.security.presentation.dto.request.UserRegistrationRequest;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.security.auth.login.LoginException;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class UserService implements UserDetailsService {
     private final SpringUserRepository SPRING_USER_REPOSITORY;
     private final PasswordEncoder PASSWORD_ENCODER;
 
-    public UserService(SpringUserRepository springUserRepository, PasswordEncoder passwordEncoder) {
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${security.jwt.expiration-in-ms}")
+    private Integer jwtExpirationInMs;
+
+    public UserService(SpringUserRepository springUserRepository,
+                       PasswordEncoder passwordEncoder
+    ) {
         this.SPRING_USER_REPOSITORY = springUserRepository;
         this.PASSWORD_ENCODER = passwordEncoder;
     }
 
-    public void registerBarOwner(String username, String password) {
-        String encodedPassword = this.PASSWORD_ENCODER.encode(password);
-        User user = new User(username, encodedPassword, UserRole.ROLE_BAR_OWNER);
-        //todo: Bar direct automatisch aanmaken, of nog niet en later zelf?
+    public void registerBarOwner(UserRegistrationRequest userRegistrationRequest) {
+        String encodedPassword = this.PASSWORD_ENCODER.encode(userRegistrationRequest.password);
+        User user = new User(userRegistrationRequest.username, encodedPassword, UserRole.ROLE_BAR_OWNER);
         this.SPRING_USER_REPOSITORY.save(user);
+    }
+
+    public String loginUser(LoginRequest loginRequest) throws LoginException {
+        User user = (User) loadUserByUsername(loginRequest.username);
+        attemptLogin(loginRequest.password, user.getPassword());
+        return generateAuthToken(user);
+    }
+
+    private void attemptLogin(String providedPassword, String storedPassword) throws LoginException {
+        if (!isMatchingPassword(providedPassword, storedPassword)) throw new LoginException("Incorrect password");
+    }
+
+    private boolean isMatchingPassword(String providedPassword, String hashedPassword) {
+        return this.PASSWORD_ENCODER.matches(providedPassword, hashedPassword);
+    }
+
+    private String generateAuthToken(User user) {
+        return String.format("Bearer: %s", generateJWT(user));
+    }
+
+    private String generateJWT(User user) {
+        List<String> roles = extractUserRoles(user);
+        byte[] signingKey = this.jwtSecret.getBytes();
+        return Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(signingKey), SignatureAlgorithm.HS256)
+                .setHeaderParam("type", "JWT")
+                .setIssuer("com-tungstun-bar-api")
+                .setAudience("com-tungstun-bar-api")
+                .setSubject(user.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+                .claim("roles", roles)
+                .compact();
+    }
+
+    private List<String> extractUserRoles(User user) {
+        return user.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
     }
 
     @Override
--- src/main/java/com/tungstun/security/presentation/controller/UserController.java
+++ src/main/java/com/tungstun/security/presentation/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.tungstun.security.presentation.controller;
 
 import com.tungstun.security.application.UserService;
+import com.tungstun.security.presentation.dto.request.LoginRequest;
 import com.tungstun.security.presentation.dto.request.UserRegistrationRequest;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +12,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.security.auth.login.LoginException;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/register")
-public class RegistrationController {
+@RequestMapping("/api/users")
+public class UserController {
     private final UserService USER_SERVICE;
 
-    public RegistrationController(UserService userService) { this.USER_SERVICE = userService; }
+    public UserController(UserService userService) { this.USER_SERVICE = userService; }
 
-    @PostMapping
+    @PostMapping("/register")
     @ApiOperation(
             value = "Registers new user",
             notes = "Provide account information in the request body to create a new user account"
@@ -27,22 +30,23 @@ public class RegistrationController {
     public ResponseEntity<Void> register(
             @Valid @RequestBody UserRegistrationRequest userRegistrationRequest
     ) {
-        this.USER_SERVICE.registerBarOwner(
-                userRegistrationRequest.username,
-                userRegistrationRequest.password
-        );
+        this.USER_SERVICE.registerBarOwner(userRegistrationRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PostMapping(path = "/login")
-    public void login(
-//            @Valid @RequestBody UserRegistrationRequest userRegistrationRequest
-    ) {
-        System.out.println("test");
-//        this.USER_SERVICE.registerBarOwner(
-//                userRegistrationRequest.username,
-//                userRegistrationRequest.password
-//        );
-//        return new ResponseEntity<>(HttpStatus.CREATED);
+    @PostMapping("/login")
+    @ApiOperation(
+            value = "Logs in user",
+            notes = "Provide login credentials in the request body to receive a session token"
+    )
+    public ResponseEntity<Void> login(
+            @Valid @RequestBody LoginRequest loginRequest
+    ) throws LoginException {
+        String jwt = this.USER_SERVICE.loginUser(loginRequest);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Authorization", jwt);
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(null);
     }
 }
