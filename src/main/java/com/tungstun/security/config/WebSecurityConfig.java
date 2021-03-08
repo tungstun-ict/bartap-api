2398c30fa59797456d0eaf9f18bc2fa36176a66f
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-08 11:40:10 +0100

    Rename websecurity config

--- src/main/java/com/tungstun/security/config/WebSecurityConfig.java
+++ src/main/java/com/tungstun/security/config/WebSecurityConfig.java
@@ -1,25 +1,16 @@
-package com.tungstun.security;
+package com.tungstun.security.config;
 
 import com.tungstun.security.presentation.filter.JwtAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true,
-        jsr250Enabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public final static String LOGIN_PATH = "/api/login";
     public final static String REGISTER_PATH = "/api/register";
     private final static String[] SWAGGER_PATHS = {
