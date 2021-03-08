c335d4b22ce010d6c953c5290b3c7ff95a68cde9
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-08 11:42:12 +0100

    Added exception handler for acces denied exceptions

--- src/main/java/com/tungstun/barapi/presentation/controllers/ExceptionHandlerController.java
+++ src/main/java/com/tungstun/barapi/presentation/controllers/ExceptionHandlerController.java
@@ -4,6 +4,7 @@ import com.sun.jdi.request.DuplicateRequestException;
 import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -19,15 +20,40 @@ public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
      * Handles self specified exceptions*/
     @ExceptionHandler(
             value = {
-                NotFoundException.class,
-                InvalidAttributesException.class,
-                DuplicateRequestException.class
+                    NotFoundException.class,
             }
     )
     public ResponseEntity<Map<String, String>> mainHandler(Exception e) {
         HashMap<String, String> map = new HashMap<>();
         map.put("Error",  e.getMessage());
         return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Handles invalid argument input / request exceptions*/
+    @ExceptionHandler(
+            value = {
+                    InvalidAttributesException.class,
+                    DuplicateRequestException.class
+            }
+    )
+    public ResponseEntity<Map<String, String>> invalidArgsHandler(Exception e) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Error",  e.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.CONFLICT);
+    }
+
+    /**
+     * Handles security exceptions*/
+    @ExceptionHandler(
+            value = {
+                    AccessDeniedException.class,
+            }
+    )
+    public ResponseEntity<Map<String, String>> securityAccessHandler(Exception e) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Error",  e.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.FORBIDDEN);
     }
 
     /**
