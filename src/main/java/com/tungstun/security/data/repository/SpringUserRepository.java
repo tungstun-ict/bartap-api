99c402bc2eb8a377e9a1a0bbeb3b8940b01f19fc
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-08 11:39:12 +0100

    move SpringUserRepository to repository package

--- src/main/java/com/tungstun/security/data/repository/SpringUserRepository.java
+++ src/main/java/com/tungstun/security/data/repository/SpringUserRepository.java
@@ -1,5 +1,6 @@
-package com.tungstun.security.data;
+package com.tungstun.security.data.repository;
 
+import com.tungstun.security.data.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
