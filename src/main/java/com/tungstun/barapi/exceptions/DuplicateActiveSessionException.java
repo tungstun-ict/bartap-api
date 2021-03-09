14559ed6994310c06fab7aee277f0cc55bef56cb
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-09 11:36:23 +0100

    Rename ActiveSessionException

--- src/main/java/com/tungstun/barapi/exceptions/DuplicateActiveSessionException.java
+++ src/main/java/com/tungstun/barapi/exceptions/DuplicateActiveSessionException.java
@@ -1,9 +1,9 @@
 package com.tungstun.barapi.exceptions;
 
-public class AlreadyActiveSessionException extends RuntimeException {
+public class DuplicateActiveSessionException extends RuntimeException {
     private final String message;
 
-    public AlreadyActiveSessionException(String message) {
+    public DuplicateActiveSessionException(String message) {
         this.message = message;
     }
 
