b82e2abde3b188429bdac978a9a35e8437b064b1
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-10 00:08:55 +0100

    Add schema and dummy_data .sql

---
+++ development/data/dummy_data.sql
@@ -0,0 +1,228 @@
+insert into "user"(id, password, username)
+values (1, '$2a$10$FAmWZOw692jCu0bkdVkj3ecSqlBqijjj5I3QMznGp2uLYiQXGr5hW', 'bart'),
+       (2, '$2a$10$FAmWZOw692jCu0bkdVkj3ecSqlBqijjj5I3QMznGp2uLYiQXGr5hW', 'user');
+
+insert into user_bar_authorization(id, bar_id, role, user_id)
+values (1, 1, 'ROLE_BAR_OWNER', 1),
+       (2, 2, 'ROLE_BAR_OWNER', 2),
+       (3, 3, 'ROLE_BAR_OWNER', 2),
+       (4, 4, 'ROLE_BAR_OWNER', 2),
+       (5, 5, 'ROLE_BAR_OWNER', 2);
+
+insert into user_user_bar_authorizations (user_id, user_bar_authorizations_id)
+values (1, 1),
+       (2, 2),
+       (2, 3),
+       (2, 4),
+       (2, 5);
+
+insert into bar(id, address, mail, name, phone_number)
+
+VALUES  (1, 'Dorpsstraat 192', 'dorpsstraat@staat.nl', 'Bartjes Bar', '1122334455'),
+        (2, 'Molenstraat 15', 'molenstraat@staat.nl', 'De Kerker Bar', '7812344562'),
+        (3, 'Kerkstraat 6', 'kerkstraat@staat.nl', 'De Kerker Bar', '0987654321'),
+        (4, 'Schoolstraat 1a', 'schoolstraat@staat.nl', 'De School Pub', '1234567890'),
+        (5, 'Molenweg 1', 'molenweg@staat.nl', 'De Molen', '6677889900');
+
+insert into person (id, name, user_id)
+values (1, 'Bart Schouten', 1),
+       (2, 'Jort Willemsen', null),
+       (3, 'Jona Leeflang', null),
+       (4, 'Sep de Geus', null),
+       (5, 'Fee Sanders', null),
+       (6, 'Test User', 2);
+
+insert into bartender(user_id)
+values (1),
+       (6);
+
+insert into customer(phone_number, user_id)
+values ('0912897344', 2),
+       ('4567890044', 3),
+       ('0632334233', 4),
+       ('0633144444', 5);
+
+insert into bar_people(bar_id, people_id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5);
+
+insert into category(id, name, product_type)
+VALUES (1, 'Bier', 'DRINK'),
+       (2, 'Wijn', 'DRINK'),
+       (3, 'Sterk', 'DRINK'),
+       (4, 'Fris', 'DRINK'),
+       (5, 'Laag alcohol', 'DRINK'),
+       (6, 'Schnaps', 'DRINK'),
+       (7, 'Shots', 'DRINK'),
+       (8, 'Mix', 'DRINK'),
+       (9, 'Snacks', 'FOOD'),
+       (10, 'Chips', 'FOOD'),
+       (11, 'Snoep', 'FOOD'),
+       (12, 'Nootjes', 'FOOD'),
+       (13, 'Gerechten', 'FOOD'),
+       (14, 'Kaas', 'FOOD'),
+       (15, 'Art suplies', 'OTHER'),
+       (16, 'Dremmles', 'OTHER'),
+       (17, 'Go Back, I Want To be MONKE', 'OTHER'),
+       (18, 'Bier', 'DRINK'),
+       (19, 'Wijn', 'DRINK'),
+       (20, 'Sterk', 'DRINK'),
+       (21, 'Fris', 'DRINK'),
+       (22, 'Laag alcohol', 'DRINK'),
+       (23, 'Schnaps', 'DRINK'),
+       (24, 'Shots', 'DRINK'),
+       (25, 'Mix', 'DRINK');
+
+
+insert into bar_categories(bar_id, categories_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (1, 9),
+       (1, 10),
+       (1, 11),
+       (1, 12),
+       (1, 13),
+       (1, 14),
+       (1, 15),
+       (1, 16),
+       (1, 17),
+       (2, 18),
+       (2, 19),
+       (3, 20),
+       (3, 21),
+       (4, 22),
+       (4, 23),
+       (5, 24),
+       (5, 25);
+
+insert into product(id, brand, is_favorite, name, price, product_type, size, category_id)
+VALUES (1, 'Heiniken', true, 'Pils vaasje', 1.30, 'DRINK', 250, 1),
+       (2, 'Heiniken', true, 'Pils glas', 1.60, 'DRINK', 330, 1),
+       (3, 'Heiniken', false, 'Pils pitcher', 9.20, 'DRINK', 1800, 1),
+       (4, 'Apple Bandit', false, 'Apple cider Juicy Apple', 1.50, 'DRINK', 300, 1),
+       (5, 'Stoney Creek', true, 'Shiraz', 1.80, 'DRINK', 200, 2),
+       (6, 'Stoney Creek', false, 'Chardonnay', 1.80, 'DRINK', 200, 2),
+       (7, 'Johney Walker', false, 'Red Label', 2.10, 'DRINK', 100, 3),
+       (8, 'Absolut', false, 'Vodka ', 2.00, 'DRINK', 0.1, 7),
+       (9, 'Bartjes Bar', true, 'Watermelon Dream ', 2.20, 'DRINK',330, 8),
+       (10, 'Coca Cola', false, 'Cola ', 1.50, 'DRINK', 100, 7),
+       (11, 'Lays', false, 'Paprika chips ', 3.00, 'FOOD', 350, 10),
+       (12, 'Duyvis', false, 'Nootjes ', 2.30, 'FOOD', 230, 12),
+       (13, 'Bartjes Bar', false, 'GO BACK TO MONKE: Special ', 2.00, 'OTHER', 0.1, 17);
+
+insert into bar_products (bar_id, products_id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (1, 9),
+       (1, 10),
+       (1, 11),
+       (1, 12),
+       (1, 13);
+
+insert into session(id, name, creation_date, closed_date, locked)
+values (1, 'Eerst sessie', date('2021-03-06 22:45:00.0'), date('2021-03-07 01:45:00.0'), true),
+       (2, 'Tweede sessie', date('2021-03-07 22:45:00.0'), date('2021-03-08 01:45:00.0'), true),
+       (3, 'Derde sessie', date('2021-03-08 22:45:00.0'), date('2021-03-09 01:45:00.0'), false),
+       (4, 'Vierde sessie', date('2021-03-09 22:45:00.0'), null, false);
+
+insert into session_bartender (user_id, id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4);
+
+insert into bar_sessions (bar_id, sessions_id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4);
+
+insert into bill(id, is_payed, customer_id, session_id)
+values (1, true, 2, 1),
+       (2, true, 3, 1),
+       (3, true, 3, 2),
+       (4, true, 4, 2),
+       (5, false, 5, 2),
+       (6, true, 2, 3),
+       (7, true, 3, 3),
+       (8, false, 4, 3),
+       (9, false, 5, 3),
+       (10, false, 2, 4),
+       (11, false, 4, 4);
+
+insert into order(id, amount, creation_date, bartender_user_id, product_id)
+values (1, 1, date('2021-03-06 22:50:00.0'), 1, 1),
+       (2, 3, date('2021-03-06 22:55:00.0'), 1, 1),
+       (3, 2, date('2021-03-06 23:00:00.0'), 1, 1),
+       (4, 5, date('2021-03-06 23:05:00.0'), 1, 1),
+       (5, 1, date('2021-03-06 23:10:00.0'), 1, 4),
+       (6, 2, date('2021-03-06 23:15:00.0'), 1, 1),
+       (7, 1, date('2021-03-06 23:20:00.0'), 1, 7),
+       (8, 1, date('2021-03-06 23:25:00.0'), 1, 4),
+       (9, 2, date('2021-03-06 23:30:00.0'), 1, 1),
+
+       (10, 2, date('2021-03-06 23:35:00.0'), 1, 1),
+       (11, 1, date('2021-03-06 23:40:00.0'), 1, 10),
+       (12, 5, date('2021-03-06 23:45:00.0'), 1, 1),
+       (13, 1, date('2021-03-06 23:50:00.0'), 1, 1),
+       (14, 4, date('2021-03-06 23:55:00.0'), 1, 12),
+       (15, 2, date('2021-03-07 00:00:00.0'), 1, 1),
+       (16, 1, date('2021-03-07 00:00:00.0'), 1, 1),
+
+       (17, 5, date('2021-03-07 23:30:00.0'), 1, 1),
+       (18, 1, date('2021-03-07 23:35:00.0'), 1, 4),
+       (19, 1, date('2021-03-07 23:40:00.0'), 1, 7),
+
+       (20, 1, date('2021-03-09 22:50:00.0'), 1, 1),
+       (21, 3, date('2021-03-09 22:55:00.0'), 1, 1),
+       (22, 2, date('2021-03-09 23:00:00.0'), 1, 1),
+       (23, 5, date('2021-03-09 23:05:00.0'), 1, 1),
+       (24, 1, date('2021-03-09 23:10:00.0'), 1, 4),
+       (25, 2, date('2021-03-09 23:15:00.0'), 1, 1),
+       (26, 1, date('2021-03-09 23:20:00.0'), 1, 7),
+       (27, 1, date('2021-03-09 23:25:00.0'), 1, 4),
+       (28, 2, date('2021-03-09 23:30:00.0'), 1, 1);
+
+insert into bill_orders(bill_id, orders_id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (1, 9),
+
+       (2, 10),
+       (2, 11),
+       (2, 12),
+       (2, 13),
+       (2, 14),
+       (2, 15),
+       (2, 16),
+
+       (10, 20),
+       (10, 21),
+       (11, 22),
+       (10, 23),
+       (10, 24),
+       (11, 25),
+       (11, 26),
+       (11, 27),
+       (10, 28);
--- development/db/schema.sql
+++ development/db/schema.sql
@@ -1,251 +1,274 @@
-create sequence hibernate_sequence start 1 increment 1;
 
-    create table bar (
-       id int8 not null,
-        address varchar(255),
-        mail varchar(255),
-        name varchar(255),
-        phone_number varchar(255),
-        primary key (id)
-    );
+    create table "user"
+(
+    id       bigint not null
+        constraint user_pkey
+            primary key,
+    password varchar(255),
+    role     varchar(255),
+    username varchar(255)
+);
 
-    create table bar_categories (
-       bar_id int8 not null,
-        categories_id int8 not null
-    );
 
-    create table bar_people (
-       bar_id int8 not null,
-        people_id int8 not null
-    );
+alter table "user"
+    owner to "bar-api-user";
 
-    create table bar_products (
-       bar_id int8 not null,
-        products_id int8 not null
-    );
+    create table user_bar_authorization
+(
+    id      bigint not null
+        constraint user_bar_authorization_pkey
+            primary key,
+    bar_id  bigint,
+    role    varchar(255),
+    user_id bigint
+);
 
-    create table bar_sessions (
-       bar_id int8 not null,
-        sessions_id int8 not null
-    );
+create table bar
+(
+    id           bigint not null
+        constraint bar_pkey
+            primary key,
+    address      varchar(255),
+    mail         varchar(255),
+    name         varchar(255),
+    phone_number varchar(255)
+);
 
-    create table bartender (
-       user_id int8 not null,
-        primary key (user_id)
-    );
+alter table bar
+    owner to "bar-api-user";
 
-    create table bill (
-       id int8 not null,
-        is_payed boolean,
-        customer_id int8,
-        session_id int8 not null,
-        primary key (id)
-    );
+create table category
+(
+    id           bigint not null
+        constraint category_pkey
+            primary key,
+    name         varchar(255),
+    product_type varchar(255)
+);
 
-    create table "bill_orders" (
-       bill_id int8 not null,
-        "orders_id" int8 not null
-    );
+alter table category
+    owner to "bar-api-user";
 
-    create table category (
-       id int8 not null,
-        name varchar(255),
-        product_type varchar(255),
-        primary key (id)
-    );
 
-    create table customer (
-       phone_number varchar(255),
-        user_id int8 not null,
-        primary key (user_id)
-    );
+create table bar_categories
+(
+    bar_id        bigint not null
+        constraint fkb6wvu7vgbvvyjn2ycvy2iyjc7
+            references bar,
+    categories_id bigint not null
+        constraint uk_hlkhodyyl4d9x1sgs76p49a1g
+            unique
+        constraint fkflspv66n9geafj6wyobuvjn3n
+            references category
+);
 
-    create table "order" (
-       id int8 not null,
-        amount int4,
-        creation_date timestamp,
-        bartender_user_id int8,
-        product_id int8,
-        primary key (id)
-    );
+alter table bar_categories
+    owner to "bar-api-user";
 
-    create table person (
-       id int8 not null,
-        name varchar(255),
-        "user_id" int8,
-        primary key (id)
-    );
 
-    create table product (
-       id int8 not null,
-        brand varchar(255),
-        is_favorite boolean,
-        name varchar(255),
-        price float8,
-        product_type varchar(255),
-        size float8,
-        category_id int8,
-        primary key (id)
-    );
 
-    create table session (
-       id int8 not null,
-        closed_date timestamp,
-        creation_date timestamp,
-        locked boolean not null,
-        primary key (id)
-    );
 
-    create table session_bartender (
-       user_id int8 not null,
-        id int8 not null
-    );
 
-    create table "user" (
-       id int8 not null,
-        password varchar(255),
-        role varchar(255),
-        username varchar(255),
-        primary key (id)
-    );
+create table person
+(
+    id      bigint not null
+        constraint person_pkey
+            primary key,
+    name    varchar(255),
+    user_id bigint
+        constraint fkdunok34g8d08mu7mtocmhfptn
+            references "user"
+);
 
-    create table user_bar_authorization (
-       id int8 not null,
-        bar_id int8,
-        role varchar(255),
-        user_id int8,
-        primary key (id)
-    );
+alter table person
+    owner to "bar-api-user";
 
-    create table "user_user_bar_authorizations" (
-       "user_id" int8 not null,
-        user_bar_authorizations_id int8 not null
-    );
+create table bartender
+(
+    user_id bigint not null
+        constraint bartender_pkey
+            primary key
+        constraint fkrriold829ilpiungxpau2iomq
+            references person
+);
 
-    alter table if exists bar_categories 
-       add constraint UK_hlkhodyyl4d9x1sgs76p49a1g unique (categories_id);
+alter table bartender
+    owner to "bar-api-user";
 
-    alter table if exists bar_people 
-       add constraint UK_645i581qjajxmhayi7x7vcl1q unique (people_id);
 
-    alter table if exists bar_products 
-       add constraint UK_ijgjfo3wjhd4uyfhryhm8byr6 unique (products_id);
 
-    alter table if exists bar_sessions 
-       add constraint UK_a8iu547r18g8iregmbx7rj1aa unique (sessions_id);
+create table customer
+(
+    phone_number varchar(255),
+    user_id      bigint not null
+        constraint customer_pkey
+            primary key
+        constraint fk3hnutqe63n4hp8qda9vp5yxd7
+            references person
+);
 
-    alter table if exists "bill_orders" 
-       add constraint UK_eq6noll9kl3m9xelx1hho1rr6 unique ("orders_id");
+alter table customer
+    owner to "bar-api-user";
 
-    alter table if exists "user_user_bar_authorizations" 
-       add constraint UK_7orid7iw1ljt2renofrt7uqnk unique (user_bar_authorizations_id);
 
-    alter table if exists bar_categories 
-       add constraint FKflspv66n9geafj6wyobuvjn3n 
-       foreign key (categories_id) 
-       references category;
+create table bar_people
+(
+    bar_id    bigint not null
+        constraint fktitff4pi26msucas9die8d4w9
+            references bar,
+    people_id bigint not null
+        constraint uk_645i581qjajxmhayi7x7vcl1q
+            unique
+        constraint fk3kykcllirre3g54vg13p74a6e
+            references person
+);
+alter table bar_people
+    owner to "bar-api-user";
 
-    alter table if exists bar_categories 
-       add constraint FKb6wvu7vgbvvyjn2ycvy2iyjc7 
-       foreign key (bar_id) 
-       references bar;
+create table session
+(
+    id            bigint  not null
+        constraint session_pkey
+            primary key,
+    closed_date   timestamp,
+    creation_date timestamp,
+    locked        boolean not null,
+    name          varchar(255)
+);
 
-    alter table if exists bar_people 
-       add constraint FK3kykcllirre3g54vg13p74a6e 
-       foreign key (people_id) 
-       references person;
 
-    alter table if exists bar_people 
-       add constraint FKtitff4pi26msucas9die8d4w9 
-       foreign key (bar_id) 
-       references bar;
+    create table product
+(
+    id           bigint not null
+        constraint product_pkey
+            primary key,
+    brand        varchar(255),
+    is_favorite  boolean,
+    name         varchar(255),
+    price        double precision,
+    product_type varchar(255),
+    size         double precision,
+    category_id  bigint
+        constraint fk1mtsbur82frn64de7balymq9s
+            references category
+);
 
-    alter table if exists bar_products 
-       add constraint FK4jnkvxrq1beu5ub8rrnjfeipe 
-       foreign key (products_id) 
-       references product;
 
-    alter table if exists bar_products 
-       add constraint FKqj2f4i2vn2nkvyw5813280not 
-       foreign key (bar_id) 
-       references bar;
+    create table bar_products
+(
+    bar_id      bigint not null
+        constraint fkqj2f4i2vn2nkvyw5813280not
+            references bar,
+    products_id bigint not null
+        constraint uk_ijgjfo3wjhd4uyfhryhm8byr6
+            unique
+        constraint fk4jnkvxrq1beu5ub8rrnjfeipe
+            references product
+);
 
-    alter table if exists bar_sessions 
-       add constraint FKldkxmr8ohrf4g8irv56lo483i 
-       foreign key (sessions_id) 
-       references session;
+alter table bar_products
+    owner to "bar-api-user";
 
-    alter table if exists bar_sessions 
-       add constraint FKrsat47b1vob5rv5g7onn93c0b 
-       foreign key (bar_id) 
-       references bar;
 
-    alter table if exists bartender 
-       add constraint FKrriold829ilpiungxpau2iomq 
-       foreign key (user_id) 
-       references person;
+alter table session
+    owner to "bar-api-user";
+    create table session_bartender
+(
+    user_id bigint not null
+        constraint fkdwtnwd7rj67yrria9seq3n9km
+            references bartender,
+    id      bigint not null
+        constraint fks9hrjajhvlbe354djdr4nus4a
+            references session
+);
 
-    alter table if exists bill 
-       add constraint FKcdveik90g4pvk7m249scu73pg 
-       foreign key (customer_id) 
-       references customer;
+alter table session_bartender
+    owner to "bar-api-user";
 
-    alter table if exists bill 
-       add constraint FKnpdke1pihy8cr0lddxntf4hl2 
-       foreign key (session_id) 
-       references session;
+create table bar_sessions
+(
+    bar_id      bigint not null
+        constraint fkrsat47b1vob5rv5g7onn93c0b
+            references bar,
+    sessions_id bigint not null
+        constraint uk_a8iu547r18g8iregmbx7rj1aa
+            unique
+        constraint fkldkxmr8ohrf4g8irv56lo483i
+            references session
+);
 
-    alter table if exists "bill_orders" 
-       add constraint FK1b0lpoex8ikeh2vm2k5b6022 
-       foreign key ("orders_id") 
-       references "order";
+alter table bar_sessions
+    owner to "bar-api-user";
 
-    alter table if exists "bill_orders" 
-       add constraint FK5cux4tsugmy29yb7mutwceibl 
-       foreign key (bill_id) 
-       references bill;
+    create table bill
+(
+    id          bigint not null
+        constraint bill_pkey
+            primary key,
+    is_payed    boolean,
+    customer_id bigint
+        constraint fkcdveik90g4pvk7m249scu73pg
+            references customer,
+    session_id  bigint not null
+        constraint fknpdke1pihy8cr0lddxntf4hl2
+            references session
+);
 
-    alter table if exists customer 
-       add constraint FK3hnutqe63n4hp8qda9vp5yxd7 
-       foreign key (user_id) 
-       references person;
+alter table bill
+    owner to "bar-api-user";
 
-    alter table if exists "order" 
-       add constraint FK65ngp9nfr0jp3nrta80k7g3vu 
-       foreign key (bartender_user_id) 
-       references bartender;
 
-    alter table if exists "order" 
-       add constraint FKknjaoi59nxmpxhr452bj95tgg 
-       foreign key (product_id) 
-       references product;
+create table "order"
+(
+    id                bigint not null
+        constraint order_pkey
+            primary key,
+    amount            integer,
+    creation_date     timestamp,
+    bartender_user_id bigint
+        constraint fk65ngp9nfr0jp3nrta80k7g3vu
+            references bartender,
+    product_id        bigint
+        constraint fkknjaoi59nxmpxhr452bj95tgg
+            references product
+);
 
-    alter table if exists person 
-       add constraint FKdunok34g8d08mu7mtocmhfptn 
-       foreign key ("user_id") 
-       references "user";
+alter table "order"
+    owner to "bar-api-user";
 
-    alter table if exists product 
-       add constraint FK1mtsbur82frn64de7balymq9s 
-       foreign key (category_id) 
-       references category;
+create table bill_orders
+(
+    bill_id   bigint not null
+        constraint fk5cux4tsugmy29yb7mutwceibl
+            references bill,
+    orders_id bigint not null
+        constraint uk_eq6noll9kl3m9xelx1hho1rr6
+            unique
+        constraint fk1b0lpoex8ikeh2vm2k5b6022
+            references "order"
+);
 
-    alter table if exists session_bartender 
-       add constraint FKs9hrjajhvlbe354djdr4nus4a 
-       foreign key (id) 
-       references session;
+alter table bill_orders
+    owner to "bar-api-user";
 
-    alter table if exists session_bartender 
-       add constraint FKdwtnwd7rj67yrria9seq3n9km 
-       foreign key (user_id) 
-       references bartender;
 
-    alter table if exists "user_user_bar_authorizations" 
-       add constraint FKjbfxkryjvkflmaph6jg9yoouk 
-       foreign key (user_bar_authorizations_id) 
-       references user_bar_authorization;
 
-    alter table if exists "user_user_bar_authorizations" 
-       add constraint FKbk2srn0hyaaxfnruc5ll5qwdo 
-       foreign key ("user_id") 
-       references "user";
+
+
+alter table user_bar_authorization
+    owner to "bar-api-user";
+
+create table user_user_bar_authorizations
+(
+    user_id                    bigint not null
+        constraint fkbk2srn0hyaaxfnruc5ll5qwdo
+            references "user",
+    user_bar_authorizations_id bigint not null
+        constraint uk_7orid7iw1ljt2renofrt7uqnk
+            unique
+        constraint fkjbfxkryjvkflmaph6jg9yoouk
+            references user_bar_authorization
+);
+
+alter table user_user_bar_authorizations
+    owner to "bar-api-user";
