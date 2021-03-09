8c6e34074562c077932f330ffe08b1a8000cf7b7
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-09 14:05:59 +0100

    Rename LockedSessionException

--- src/main/java/com/tungstun/barapi/exceptions/LockedSessionException.java
+++
@@ -1,9 +0,0 @@
-package com.tungstun.barapi.exceptions;
-
-public class LockedSessionException extends RuntimeException {
-    public LockedSessionException() { }
-
-    public LockedSessionException(String message) {
-        super(message);
-    }
-}
