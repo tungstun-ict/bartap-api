23deafbd9b924cd9e007bea6697003218c847d99
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-09 14:50:06 +0100

    Make barauth directly working
    BarApiPermissionEvaluator checks stored barauth
    instead of jwt stored barAuth, so user can
    instantly make calls to made resource

--- src/main/java/com/tungstun/security/config/BarApiGlobalSecurityConfig.java
+++ src/main/java/com/tungstun/security/config/BarApiGlobalSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.tungstun.security.config;
 
+import com.tungstun.security.application.UserService;
 import com.tungstun.security.config.evaluator.BarApiPermissionEvaluator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,11 +20,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
         securedEnabled = true,
         jsr250Enabled = true)
 public class BarApiGlobalSecurityConfig extends GlobalMethodSecurityConfiguration {
+    private final UserService USER_SERVICE;
+
+    public BarApiGlobalSecurityConfig(@Lazy UserService USER_SERVICE) {
+        this.USER_SERVICE = USER_SERVICE;
+    }
+
     @Override
     protected MethodSecurityExpressionHandler createExpressionHandler() {
         DefaultMethodSecurityExpressionHandler expressionHandler =
                 new DefaultMethodSecurityExpressionHandler();
-        expressionHandler.setPermissionEvaluator(new BarApiPermissionEvaluator());
+        expressionHandler.setPermissionEvaluator(new BarApiPermissionEvaluator(this.USER_SERVICE));
         return expressionHandler;
     }
 
--- src/main/java/com/tungstun/security/config/evaluator/BarApiPermissionEvaluator.java
+++ src/main/java/com/tungstun/security/config/evaluator/BarApiPermissionEvaluator.java
@@ -1,19 +1,30 @@
 package com.tungstun.security.config.evaluator;
 
+import com.tungstun.security.application.UserService;
+import com.tungstun.security.data.model.User;
 import com.tungstun.security.data.model.UserProfile;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
+@Component
 public class BarApiPermissionEvaluator implements PermissionEvaluator {
+    private final UserService USER_SERVICE;
+
+    public BarApiPermissionEvaluator(UserService userService) {
+        this.USER_SERVICE = userService;
+    }
+
     @Override
     public boolean hasPermission(Authentication auth, Object targetDomainObject, Object permission) {
         if ((auth == null) || (targetDomainObject == null) || !(permission instanceof String)){
             return false;
         }
         Long barId = (Long) targetDomainObject;
-        return hasPrivilege(auth, barId, permission.toString().toUpperCase());
+        String username = ((UserProfile) auth.getPrincipal()).getUsername();
+        return hasPrivilege(username, barId, permission.toString().toUpperCase());
     }
 
     @Override
@@ -21,12 +32,13 @@ public class BarApiPermissionEvaluator implements PermissionEvaluator {
         if ((auth == null) || (targetType == null) || !(permission instanceof String)) {
             return false;
         }
-        return hasPrivilege(auth, Long.valueOf((String) targetId), permission.toString().toUpperCase());
+        String username = ((UserProfile) auth.getPrincipal()).getUsername();
+        return hasPrivilege(username, Long.valueOf((String) targetId), permission.toString().toUpperCase());
     }
 
-    private boolean hasPrivilege(Authentication auth, Long barId, String permission) {
-        UserProfile userProfile = (UserProfile) auth.getPrincipal();
-        String role = userProfile.getBarAuthorization().get(barId);
+    private boolean hasPrivilege(String username, Long barId, String permission) {
+        User user = (User) this.USER_SERVICE.loadUserByUsername(username);
+        String role = user.getAuthoritiesMap().get(barId);
         return role != null && role.equals(permission);
     }
 }
