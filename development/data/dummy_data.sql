4645ec0f205458f782a0d47072bf8e553797893f
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-19 12:11:39 +0100

    Update schema and dummy data

--- development/data/dummy_data.sql
+++ development/data/dummy_data.sql
@@ -1,6 +1,7 @@
 insert into "user"(id, password, username, mail, first_name, last_name)
-values (1, '$2a$10$FAmWZOw692jCu0bkdVkj3ecSqlBqijjj5I3QMznGp2uLYiQXGr5hW', 'bart', 'bart@mail.com', 'Bart', 'Schouten'),
-       (2, '$2a$10$FAmWZOw692jCu0bkdVkj3ecSqlBqijjj5I3QMznGp2uLYiQXGr5hW', 'user', 'test@mail.com', 'testFirst', 'testLast');
+values (1, '$2a$10$FAmWZOw692jCu0bkdVkj3ecSqlBqijjj5I3QMznGp2uLYiQXGr5hW', 'admin', 'admin@mail.com', 'Admin', 'aDMIN'),
+       (2, '$2a$10$FAmWZOw692jCu0bkdVkj3ecSqlBqijjj5I3QMznGp2uLYiQXGr5hW', 'user', 'test@mail.com', 'testFirst', 'testLast'),
+       (3, '$2a$04$dKIufHL4vTOUG7e2f4.plu3tHUqbGdhuf14Ss.GZyuPUTOUp.r4wi', 'Bart', 'bartschouten02@live.nl', 'Bart', 'Schouten');
 
 insert into user_bar_authorization(id, bar_id, role, user_id)
 values (1, 1, 'ROLE_BAR_OWNER', 1),
@@ -10,7 +11,7 @@ values (1, 1, 'ROLE_BAR_OWNER', 1),
        (5, 5, 'ROLE_BAR_OWNER', 2);
 
 insert into user_user_bar_authorizations (user_id, user_bar_authorizations_id)
-values (1, 1),
+values (3, 1),
        (2, 2),
        (2, 3),
        (2, 4),
@@ -25,7 +26,7 @@ VALUES  (1, 'Dorpsstraat 192', 'dorpsstraat@staat.nl', 'Bartjes Bar', '112233445
         (5, 'Molenweg 1', 'molenweg@staat.nl', 'De Molen', '6677889900');
 
 insert into person (id, name, phone_number, user_id)
-values (1, 'Bart Schouten', '66772323332', 1),
+values (1, 'Bart Schouten', '66772323332', 3),
        (2, 'Jort Willemsen', '0912897344', null),
        (3, 'Jona Leeflang', '4567890044', null),
        (4, 'Sep de Geus', '0632334233', null),
@@ -95,20 +96,20 @@ VALUES (1, 1),
        (5, 24),
        (5, 25);
 
-insert into product(id, brand, is_favorite, name, price, product_type, size, category_id)
-VALUES (1, 'Heiniken', true, 'Pils vaasje', 1.30, 'DRINK', 250, 1),
-       (2, 'Heiniken', true, 'Pils glas', 1.60, 'DRINK', 330, 1),
-       (3, 'Heiniken', false, 'Pils pitcher', 9.20, 'DRINK', 1800, 1),
-       (4, 'Apple Bandit', false, 'Apple cider Juicy Apple', 1.50, 'DRINK', 300, 1),
-       (5, 'Stoney Creek', true, 'Shiraz', 1.80, 'DRINK', 200, 2),
-       (6, 'Stoney Creek', false, 'Chardonnay', 1.80, 'DRINK', 200, 2),
-       (7, 'Johney Walker', false, 'Red Label', 2.10, 'DRINK', 100, 3),
-       (8, 'Absolut', false, 'Vodka ', 2.00, 'DRINK', 0.1, 7),
-       (9, 'Bartjes Bar', true, 'Watermelon Dream ', 2.20, 'DRINK',330, 8),
-       (10, 'Coca Cola', false, 'Cola ', 1.50, 'DRINK', 100, 7),
-       (11, 'Lays', false, 'Paprika chips ', 3.00, 'FOOD', 350, 10),
-       (12, 'Duyvis', false, 'Nootjes ', 2.30, 'FOOD', 230, 12),
-       (13, 'Bartjes Bar', false, 'GO BACK TO MONKE: Special ', 2.00, 'OTHER', 0.1, 17);
+insert into product(id, brand, is_favorite, name, price, size, category_id)
+VALUES (1, 'Heiniken', true, 'Pils vaasje', 1.30, 250, 1),
+       (2, 'Heiniken', true, 'Pils glas', 1.60, 330, 1),
+       (3, 'Heiniken', false, 'Pils pitcher', 9.20, 1800, 1),
+       (4, 'Apple Bandit', false, 'Apple cider Juicy Apple', 1.50, 300, 1),
+       (5, 'Stoney Creek', true, 'Shiraz', 1.80, 200, 2),
+       (6, 'Stoney Creek', false, 'Chardonnay', 1.80, 200, 2),
+       (7, 'Johney Walker', false, 'Red Label', 2.10, 100, 3),
+       (8, 'Absolut', false, 'Vodka ', 2.00, 0.1, 7),
+       (9, 'Bartjes Bar', true, 'Watermelon Dream ', 2.20,330, 8),
+       (10, 'Coca Cola', false, 'Cola ', 1.50, 100, 7),
+       (11, 'Lays', false, 'Paprika chips ', 3.00, 350, 10),
+       (12, 'Duyvis', false, 'Nootjes ', 2.30, 230, 12),
+       (13, 'Bartjes Bar', false, 'GO BACK TO MONKE: Special ', 2.00, 0.1, 17);
 
 insert into bar_products (bar_id, products_id)
 values (1, 1),
