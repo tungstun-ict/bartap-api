343627ccbbf5825711359bba9aac172ce7320ca2
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-07-15 13:45:41 +0200

    Add ProductSearchEngine Add JaccardStringSimilarityComparer Add product search

--- src/main/java/com/tungstun/barapi/application/StringSimilarityStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.tungstun.barapi.application;
-
-import java.util.List;
-
-public interface StringSimilarityStrategy<T>  {
-    Double apply(T entity, String compareString);
-    List<T> filterList(List<T> entities, String compareString);
-    List<T> filterList(List<T> entities, String compareString, Double threshold);
-}
