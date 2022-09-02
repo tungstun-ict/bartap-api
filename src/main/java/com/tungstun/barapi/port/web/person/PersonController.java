a58132cac21dddf1c948efe436cd014b858f4a43
ChromaChroma <jona.beer@gmail.com>
2022-09-02 21:51:19 +0200

    Update Person controller and DTO's to Springdoc

--- src/main/java/com/tungstun/barapi/port/web/person/PersonController.java
+++ src/main/java/com/tungstun/barapi/port/web/person/PersonController.java
@@ -14,8 +14,8 @@ import com.tungstun.barapi.port.web.person.response.PersonResponse;
 import com.tungstun.common.response.UuidResponse;
 import com.tungstun.security.application.user.UserQueryHandler;
 import com.tungstun.security.domain.user.User;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +23,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
@@ -49,14 +48,12 @@ public class PersonController {
     @GetMapping("/bars/{barId}/people")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasPermission(#barId, {'OWNER','BARTENDER'})")
-    @ApiOperation(
-            value = "Finds all people of bar",
-            notes = "Provide categoryId of bar to look up all people that are linked to the bar",
-            response = PersonResponse.class,
-            responseContainer = "List"
+    @Operation(
+            summary = "Finds people of a bar",
+            description = "Find all people of a bar with the given id"
     )
     public List<PersonResponse> getAllPeopleOfBar(
-            @ApiParam(value = "ID value for the bar you want to retrieve people from") @PathVariable UUID barId
+            @Parameter(description = "Id value of the bar") @PathVariable UUID barId
     ) throws EntityNotFoundException {
         List<Person> allPeople = personQueryHandler.handle(new ListPeopleOfBar(barId));
         return converter.convertAll(allPeople);
@@ -65,14 +62,13 @@ public class PersonController {
     @GetMapping(path = "/bars/{barId}/people/{personId}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasPermission(#barId, {'OWNER','BARTENDER'})")
-    @ApiOperation(
-            value = "Finds person of bar",
-            notes = "Provide categoryId of bar and person to look up a specific person of the bar",
-            response = PersonResponse.class
+    @Operation(
+            summary = "Finds person of a bar",
+            description = "Find a person of a bar with the given id's"
     )
     public PersonResponse getPersonOfBar(
-            @ApiParam(value = "ID value for the bar you want to retrieve the person from") @PathVariable("barId") UUID barId,
-            @ApiParam(value = "ID value for the person you want to retrieve") @PathVariable("personId") UUID personId
+            @Parameter(description = "Id value of the bar") @PathVariable UUID barId,
+            @Parameter(description = "Id value of the person") @PathVariable("personId") UUID personId
     ) throws EntityNotFoundException {
         Person person = personQueryHandler.handle(new GetPerson(barId, personId));
         return converter.convert(person);
@@ -80,14 +76,13 @@ public class PersonController {
 
     @PostMapping(path = "/bars/{barId}/people/{personId}/connect")
     @PreAuthorize("hasPermission(#barId, {'OWNER','BARTENDER'})")
-    @ApiOperation(
-            value = "Gets connection token of person of bar",
-            notes = "Provide id of bar and person to get a connection token that can be used to connect the person to a user",
-            response = PersonResponse.class
+    @Operation(
+            summary = "Creates a person connection token for a user",
+            description = "Creates and returns a connection token a user can use to connect with a person/customer of a bar so long no other user is connected to the person"
     )
     public ResponseEntity<Void> getPersonConnectionToken(
-            @ApiParam(value = "ID value for the bar you want to get the person's connection token from") @PathVariable("barId") UUID barId,
-            @ApiParam(value = "ID value for the person you want to retrieve the connection token from") @PathVariable("personId") UUID personId
+            @Parameter(description = "Id value of the bar") @PathVariable UUID barId,
+            @Parameter(description = "Id value of the person") @PathVariable("personId") UUID personId
     ) throws EntityNotFoundException {
         String token = personCommandHandler.handle(new CreatePersonConnectionToken(barId, personId));
 
@@ -98,14 +93,13 @@ public class PersonController {
 
     @PostMapping(path = "/connect-user")
     @ResponseStatus(HttpStatus.OK)
-//    @PreAuthorize("hasPermission(#barId, {})")
-    @ApiOperation(
-            value = "Gets connection token of person of bar",
-            notes = "Provide id of bar and person to get a connection token that can be used to connect the person to a user",
-            response = PersonResponse.class
+//    @PreAuthorize("hasPermission(#barId, {})") todo Make sure is authorized, not perse role authorized
+    @Operation(
+            summary = "Connects a user and a person",
+            description = "Connects a user with a person using the person's connection token"
     )
     public void connectUserToPerson(
-            @ApiIgnore Authentication authentication,
+            @Parameter(hidden = true) Authentication authentication,
             @RequestBody @Valid ConnectUserToPersonRequest request
     ) throws EntityNotFoundException {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
@@ -117,13 +111,12 @@ public class PersonController {
     @PostMapping("/bars/{barId}/people")
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasPermission(#barId, {'OWNER','BARTENDER'})")
-    @ApiOperation(
-            value = "Creates person for bar",
-            notes = "Provide categoryId of bar to create a new person with the information from the request body for the bar",
-            response = PersonResponse.class
+    @Operation(
+            summary = "Creates a person",
+            description = "Create a new person for a bar with the given id"
     )
     public UuidResponse createNewPersonForBar(
-            @ApiParam(value = "ID value for the bar you want to create the new person for") @PathVariable("barId") UUID barId,
+            @Parameter(description = "Id value of the bar") @PathVariable UUID barId,
             @Valid @RequestBody CreatePersonRequest request
     ) throws EntityNotFoundException {
         CreatePerson command = new CreatePerson(barId, request.name());
@@ -133,14 +126,13 @@ public class PersonController {
     @PutMapping("/bars/{barId}/people/{personId}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasPermission(#barId, {'OWNER','BARTENDER'})")
-    @ApiOperation(
-            value = "Updates the person of bar",
-            notes = "Provide categoryId of bar and person to update the person with the information from the request body",
-            response = PersonResponse.class
+    @Operation(
+            summary = "Updates a person of a bar",
+            description = "Update a person of a bar with the given information"
     )
     public UuidResponse updatePerson(
-            @ApiParam(value = "ID value for the bar you want to update the person from") @PathVariable("barId") UUID barId,
-            @ApiParam(value = "ID value for the person you want to update") @PathVariable("personId") UUID personId,
+            @Parameter(description = "Id value of the bar") @PathVariable UUID barId,
+            @Parameter(description = "Id value of the person") @PathVariable("personId") UUID personId,
             @Valid @RequestBody UpdatePersonRequest request
     ) throws EntityNotFoundException {
         UpdatePerson command = new UpdatePerson(barId, personId, request.name());
@@ -150,13 +142,13 @@ public class PersonController {
     @DeleteMapping("/bars/{barId}/people/{personId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasPermission(#barId, {'OWNER','BARTENDER'})")
-    @ApiOperation(
-            value = "Deletes the person from bar",
-            notes = "Provide categoryId of bar and person to delete the person from the bar"
+    @Operation(
+            summary = "Deletes a person from a bar",
+            description = "Delete a person of a bar with the given id's"
     )
     public void deletePerson(
-            @ApiParam(value = "ID value for the bar you want to delete the person from") @PathVariable("barId") UUID barId,
-            @ApiParam(value = "ID value for the bar you want to delete") @PathVariable("personId") UUID personId
+            @Parameter(description = "Id value of the bar") @PathVariable UUID barId,
+            @Parameter(description = "Id value of the person") @PathVariable("personId") UUID personId
     ) throws EntityNotFoundException {
         DeletePerson command = new DeletePerson(personId);
         personCommandHandler.handle(command);
