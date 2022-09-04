29731df3b155e03db5d0a06eb827e93e687b340a
ChromaChroma <jona.beer@gmail.com>
2022-09-04 17:39:48 +0200

    Add swagger public endpoints

--- src/main/java/com/tungstun/security/config/BarApiWebSecurityConfig.java
+++ src/main/java/com/tungstun/security/config/BarApiWebSecurityConfig.java
@@ -14,6 +14,14 @@ public class BarApiWebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String LOGIN_REFRESH_PATH = "/api/authenticate/refresh";
     private static final String REGISTER_PATH = "/api/register";
     private static final String[] SWAGGER_PATHS = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
             // -- Swagger UI v3 (OpenAPI)
             "/swagger-ui/**",
             "/v3/api-docs/**",
